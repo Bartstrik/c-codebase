6cce9782fc2ca1e4d67f9d07e1b4cd13e595fe11
Bart Strik <bcstrik@gmail.com>
2024-09-10 16:04:35 +0200

    gitignore update to remove executables

---
+++ src/base/input.h
@@ -0,0 +1,19 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+//todo 
+//make a template for this
+
+char get_char(const char *prompt);
+
+double get_double(const char *prompt);
+
+float get_float(const char *prompt);
+
+int get_int(const char *prompt);
+
+long get_long(const char *prompt);
+
+char* get_string(const char *prompt);
+
+#endif //INPUT_H
