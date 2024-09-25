e2a28be8990f29399c30efd5a80d529552b3f814
Bartstrik <bcstrik@gmail.com>
2024-09-25 12:58:49 +0200

    added new types

--- src/main.c
+++ src/main.c
@@ -1,29 +1,9 @@
 #include <stdio.h>
 #include "base/base.h"
 #include "base/uinput.h"
+#include "base/types.h"
 
 int main(void) {
-    //test of the get functions
-    char c;
-    double d;
-    float f;
-    int i;
-    long l;
-    char *s;
-    c = get_char("enter char : \n");
-    printf("char = %c\n", c);
-    d = get_double("enter double : \n");
-    printf("double = %lf\n", d);
-    f = get_float("enter float : \n");
-    printf("float = %f\n", f);
-    i = get_int("enter int : \n");
-    printf("int = %i\n", i);
-    l = get_long("enter long : \n");
-    printf("long = %li\n", l);
-    s = get_string("enter string : \n", 5);
-    printf("string = %s\n", s);
-
-
-    printf("OS = "STRINGIFY(OS)"\n");
-    printf("Compiler = "STRINGIFY(COMPILER)"\n");
+    printf("size of short signed int : %li\n", sizeof(i8));
+    printf("size of long double : %li\n", sizeof(f128));
 }
