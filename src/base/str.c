4ff2d2eaf7e3bc3b6ef47511bc1922bf027313e7
Bartstrik <bcstrik@gmail.com>
2024-09-27 12:39:57 +0200

    min and max macros + bugfix in str_alloc

--- src/base/str.c
+++ src/base/str.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "str.h"
+#include "math.h"
 
 
 
@@ -12,15 +13,40 @@ int str_len(str s) {
     return n;
 }
 
+str str_cmp(str s1, str s2) {
+    int len = MAX(str_len(s1), str_len(s2));
+
+    str s3 = str_alloc_len(len);
+    for (int i = 0; i < len; i++) {
+        s3[i] = s1[i] ^ s2[i];
+    }
+    return s3;
+}
+
+int str_bin_cmp(str s1, str s2) {
+    int len = MAX(str_len(s1), str_len(s2));
+    for (int i = 0; i < len; i++) {
+        if (s1[i] != s2[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 str str_alloc(str s) {
     str new_s;
     int len = str_len(s);
-    new_s = malloc(len);
+    new_s = malloc((len + 1));
 
     for (int i = 0; i < len; i++) {
         new_s[i] = s[i];
     }
+    new_s[len] = '\0'; 
     return new_s;
+}
+
+str str_alloc_len(int len) {
+    return malloc(len);
 }
 
 str str_toupper(str s, int len) {
--- src/base/str.h
+++ src/base/str.h
@@ -4,8 +4,11 @@
 
 typedef char* str;
 
-str str_alloc(str s);
 int str_len(str s);
+str str_cmp(str s1, str s2);
+int str_bin_cmp(str s1, str s2);
+str str_alloc(str s);
+str str_alloc_len(int len);
 str str_toupper(str s, int len);
 str str_tolower(str s, int len);
 #endif // str_H
